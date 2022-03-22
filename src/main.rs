7f183760f33377143b4ddb3b98c2d7b5eb95d6b8
Per Lindgren <per.lindgren@ltu.se>
2022-03-22 22:34:24 +0100

    two objects

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,126 @@
 use cro::*;
-use std::sync::Mutex;
 
 // user code
 #[derive(Debug)]
-pub struct State {
+pub struct O {
     pub i: i32,
 }
 
-impl State {
+impl O {
     pub fn new(i: i32) -> Self {
         Self { i }
     }
 
     // sync
     pub fn inc(&mut self) -> i32 {
+        println!("o inc");
         self.i += 1;
         self.i
     }
 
+    // sync
+    pub fn inc2(&mut self) -> i32 {
+        println!("o inc2");
+        self.inc()
+    }
+
     // async
     pub fn add(&mut self, v: i32) {
+        println!("o add");
         self.i += v;
     }
 }
 
 // auto generated
 #[derive(Debug)]
-pub struct Cro {
-    pub state: Resource<State>,
+pub struct Cro_O {
+    pub state: Resource<O>,
 }
 
-impl Cro {
+impl Cro_O {
     pub fn new(i: i32) -> Self {
         Self {
-            state: Resource::new(State::new(i)),
+            state: Resource::new(O::new(i)),
         }
     }
 
     // codegen for sync
     pub fn inc(&self) -> i32 {
-        self.state.sync(&State::inc)
+        self.state.sync(&O::inc)
+    }
+
+    // codegen for sync
+    pub fn inc2(&self) -> i32 {
+        self.state.sync(&O::inc2)
     }
 
     // codegen for async (message)
-    pub fn add(&self, v: i32) -> Message<State> {
+    pub fn add(&self, v: i32) -> Message<O> {
+        // should we automatically enqueue the message?
+        Message {
+            o: &self.state,
+            f: Box::new(move |state| state.add(v)),
+        }
+    }
+}
+
+// user code
+#[derive(Debug)]
+pub struct O2 {
+    pub i: i32,
+}
+
+impl O2 {
+    pub fn new(i: i32) -> Self {
+        Self { i }
+    }
+
+    // sync
+    pub fn inc(&mut self) -> i32 {
+        println!("o2 inc");
+        self.i += 1;
+        self.i
+    }
+
+    // sync
+    pub fn inc2(&mut self) -> i32 {
+        println!("o2 inc2");
+        self.inc()
+    }
+
+    // async
+    pub fn add(&mut self, v: i32) {
+        println!("o2 add");
+        self.i += v;
+    }
+}
+
+// auto generated
+#[derive(Debug)]
+pub struct Cro_O2 {
+    pub state: Resource<O2>,
+}
+
+impl Cro_O2 {
+    pub fn new(i: i32) -> Self {
+        Self {
+            state: Resource::new(O2::new(i)),
+        }
+    }
+
+    // codegen for sync
+    pub fn inc(&self) -> i32 {
+        self.state.sync(&O2::inc)
+    }
+
+    // codegen for sync
+    pub fn inc2(&self) -> i32 {
+        self.state.sync(&O2::inc2)
+    }
+
+    // codegen for async (message)
+    pub fn add(&self, v: i32) -> Message<O2> {
+        // should we automatically enqueue the message?
         Message {
             o: &self.state,
             f: Box::new(move |state| state.add(v)),
@@ -52,9 +129,21 @@ impl Cro {
 }
 
 fn main() {
-    let o = Cro::new(0);
+    let o = Cro_O::new(0);
+    let o2 = Cro_O2::new(0);
 
-    o.inc();
+    println!("{}", o.inc());
+    println!("{}", o.inc2());
+    println!("{}", o2.inc2());
+
+    let mut v: Vec<Box<dyn Runnable>> = vec![];
+    v.push(Box::new(o.add(1)));
+    v.push(Box::new(o.add(2)));
+    v.push(Box::new(o2.add(20)));
+
+    for m in v {
+        m.sync();
+    }
 
     println!("o {:?}", o);
 }
